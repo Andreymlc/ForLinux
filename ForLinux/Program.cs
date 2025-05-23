2105edfd5f53c01dd07ecbcb1418f6d5e3501881
Andrey Maltsev <zeeezya@gmail.com>
2025-05-23 23:24:19 +0300

    fix cors

--- ForLinux/Program.cs
+++ ForLinux/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAll", policy =>
     {
         policy
-            .WithOrigins()
+            .AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
