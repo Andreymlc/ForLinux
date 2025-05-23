8ca12b39b315056b2c03df210041020adf094d81
Andrey Maltsev <zeeezya@gmail.com>
2025-05-23 23:34:19 +0300

    fix cors

--- ForLinux/Program.cs
+++ ForLinux/Program.cs
@@ -12,8 +12,7 @@ builder.Services.AddCors(options =>
         policy
             .AllowAnyOrigin()
             .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials();
+            .AllowAnyHeader();
     });
 });
 
